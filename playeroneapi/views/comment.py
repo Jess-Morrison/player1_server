797381e1a40a6e97f7448e2ed6ee5d98d42460ee
Jessica Morrison <jnicomorr@gmail.com>
2023-02-11 13:50:15 -0600

    Created and tested comment view

--- playeroneapi/views/comment.py
+++ playeroneapi/views/comment.py
@@ -1,87 +1,86 @@
-# """View module for handling requests about game types"""
-# from django.http import HttpResponseServerError
-# from rest_framework.viewsets import ViewSet
-# from rest_framework.response import Response
-# from rest_framework import serializers, status
-# from playeroneapi.models import Game, Gamer, GameType
+"""View module for handling requests about game types"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from playeroneapi.models import VideoGame, User, Reaction, Comment
 
 
-# class CommentView(ViewSet):
-#     """Level up game types view"""
 
-#     def retrieve(self, request, pk):
-#         """Handle GET requests for single game type
+class CommentView(ViewSet):
+    """Level up game types view"""
 
-#         Returns:
-#             Response -- JSON serialized game type
-#         """
-#         game = Game.objects.get(pk=pk)
-#         serializer = GameSerializer(game)
-#         return Response(serializer.data)
+    def retrieve(self, request, pk):
+        """Handle GET requests for single game type
 
-#     def list(self, request):
-#         """Handle GET requests to get all game types
+        Returns:
+            Response -- JSON serialized game type
+        """
+        comment = Comment.objects.get(pk=pk)
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
 
-#         Returns:
-#             Response -- JSON serialized list of game types
-#         """
-#         games = Game.objects.all()
-#         game_type = request.query_params.get('type', None)
-#         if game_type is not None:
-#           games = games.filter(game_type_id=game_type)  
-#         serializer = GameSerializer(games, many = True)
-#         return Response(serializer.data)
+    def list(self, request):
+        """Handle GET requests to get all game types
+
+        Returns:
+            Response -- JSON serialized list of game types
+        """
+        comments = Comment.objects.all()  
+        serializer = CommentSerializer(comments, many = True)
+        return Response(serializer.data)
       
-#     def create(self, request):
-#         """Handle POST operations
+    def create(self, request):
+        """Handle POST operations
 
-#         Returns
-#             Response -- JSON serialized game instance
-#         """
-#         gamer = Gamer.objects.get(uid=request.data["user_id"])
-#         # why did I have to change this to uid?
-#         # This should probably be pk instead of uid, you are trying to grab the pk
-#         game_type = GameType.objects.get(pk=request.data["game_type"])
+        Returns
+            Response -- JSON serialized game instance
+        """
+        user = User.objects.get(pk=request.data["user"])
+        game = VideoGame.objects.get(pk=request.data["game"])
+        # reactions = Reaction.objects.get(pk=request.data["reactions"])
 
-#         game = Game.objects.create(
-#         title=request.data["title"],
-#         maker=request.data["maker"],
-#         number_of_players=request.data["number_of_players"],
-#         skill_level=request.data["skill_level"],
-#         gamer=gamer,
-#         game_type=game_type
-#         )
-#         serializer = GameSerializer(game)
-#         return Response(serializer.data)
+        comment = Comment.objects.create(
+        comment_title=request.data["comment_title"],
+        comment=request.data["comment"],
+        date_created=request.data["date_created"],
+  
+        user=user,
+        game=game,
+        # reactions=reactions
+        )
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
     
-#     def update(self, request, pk):
-#         """Handle PUT requests for a game
+    def update(self, request, pk):
+        """Handle PUT requests for a game
 
-#         Returns:
-#             Response -- Empty body with 204 status code
-#         """
+        Returns:
+            Response -- Empty body with 204 status code
+        """
 
-#         game = Game.objects.get(pk=pk)
-#         game.title = request.data["title"]
-#         game.maker = request.data["maker"]
-#         game.number_of_players = request.data["number_of_players"]
-#         game.skill_level = request.data["skill_level"]
+        comment = Comment.objects.get(pk=pk)
+        comment.comment_title = request.data["comment_title"]
+        comment.comment = request.data["comment"]
+        comment.date_created = request.data["date_created"]
 
-#         game_type = GameType.objects.get(pk=request.data["game_type"])
-#         game.game_type = game_type
-#         game.save()
+        # game = VideoGame.objects.get(pk=request.data["game"])
+        # comment.game = game
+        # reactions = Reaction.objects.get(pk=request.data["reactions"])
+        # comment.reactions = reactions
+        comment.save()
 
-#         return Response(None, status=status.HTTP_204_NO_CONTENT) 
+        return Response(None, status=status.HTTP_204_NO_CONTENT) 
     
-#     def destroy(self, request, pk):
-#         game = Game.objects.get(pk=pk)
-#         game.delete()
-#         return Response(None, status=status.HTTP_204_NO_CONTENT)
+    def destroy(self, request, pk):
+        comment = Comment.objects.get(pk=pk)
+        comment.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
     
-# class GameSerializer(serializers.ModelSerializer):
-#     """JSON serializer for game types
-#     """
-#     class Meta:
-#         model = Game
-#         fields = ('id', 'title', 'maker', 'gamer', 'number_of_players', 'skill_level', 'game_type') 
-#         depth = 1     
+class CommentSerializer(serializers.ModelSerializer):
+    """JSON serializer for game types
+    """
+    class Meta:
+        model = Comment
+        fields = ('id', 'user', 'game', 'reactions', 'comment_title', 'comment', 'date_created') 
+        depth = 1     
